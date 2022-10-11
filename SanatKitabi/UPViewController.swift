04e394befd9d722a9b20a6e5a8e4f2458607569b
baris <akturk_baris@hotmail.com>
2022-10-11 16:46:25 +0200

    görsel tıklama galeriye girme çıkma

--- SanatKitabi/UPViewController.swift
+++ SanatKitabi/UPViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class UPViewController: UIViewController {
+class UPViewController: UIViewController , UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
     
     @IBOutlet weak var selectImage: UIImageView!
@@ -18,10 +18,31 @@ class UPViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // görsel tıklmayı açma
+        selectImage.isUserInteractionEnabled = true
+        let imageRecognizer = UITapGestureRecognizer(target: self, action:  #selector(chooseImage))
+        selectImage.addGestureRecognizer(imageRecognizer)
+        
+        
+        
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(keyboarfOff))
         view.addGestureRecognizer(gestureRecognizer)
     }
 
+    // galeriden foto seçme
+    @objc func chooseImage(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker,animated: true)
+        
+    }
+    
+    // galeriden çıkma
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        selectImage.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true)
+    }
     
    // keyboard off
     @objc func keyboarfOff(){
